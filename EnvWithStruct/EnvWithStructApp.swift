07f5201eae421716d0dc4e8f8e83c20d565c3286
Derwood Freen <freenderwood@gmail.com>
2025-08-14 16:57:01 +0100

    Initial Commit

---
+++ EnvWithStruct/EnvWithStructApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvWithStructApp.swift
+//  EnvWithStruct
+//
+//  Created by me developer on 14/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct EnvWithStructApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
